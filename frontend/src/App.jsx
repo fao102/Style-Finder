b32976357cde2bb1a68cd497fa5ce993b82a9f76
Faja16 <128690750+Faja16@users.noreply.github.com>
2025-11-03 16:11:02 +0000

    added convex

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,35 @@
-import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import ImageUploader from "./components/imageUploader";
+import { SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react'
+import { Authenticated, Unauthenticated, AuthLoading } from 'convex/react'
 
 export default function App() {
   return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100 w-100 bg-light">
-      <div className="text-center w-100" style={{ maxWidth: 720 }}>
-        <h1 className="mb-4 fw-bold" style={{ letterSpacing: "1px" }}>
-          Style Finder
-        </h1>
-        <section>
-          <ImageUploader />
-        </section>
+    <main>
+      <div className="position-relative min-vh-100 w-100 bg-light">
+        <div className="position-absolute top-0 end-0 p-3">
+          <Unauthenticated>
+            <SignUpButton/>            
+            <SignInButton />
+          </Unauthenticated>
+          <Authenticated>
+            <UserButton />
+          </Authenticated>
+        </div>
+        <div className="d-flex justify-content-center align-items-center min-vh-100 w-100">
+          <div className="text-center w-100" style={{ maxWidth: 720 }}>
+            <h1 className="mb-4 fw-bold" style={{ letterSpacing: "1px" }}>
+              Style Finder
+            </h1>
+            <section>
+              <ImageUploader />
+            </section>
+          </div>
+        </div>
+        <AuthLoading>
+          <p>Still loading</p>
+        </AuthLoading>
       </div>
-    </div>
+    </main>
   );
 }
