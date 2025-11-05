dca62345971ed6055628d55845d44c02ab0fd593
fao1602 <128690750+Faja16@users.noreply.github.com>
2025-11-05 22:38:20 +0000

    finding out vite url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export async function uploadImage(formData) {
+  console.log("Uploading image to:", API_URL);
   const response = await axios.post(`${API_URL}`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
