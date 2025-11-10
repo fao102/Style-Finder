6271dc5e76d956918a9d88360a5933250bfdaf05
fao1602 <128690750+Faja16@users.noreply.github.com>
2025-11-10 16:36:04 +0000

    fixing ports and axios.pot

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export async function uploadImage(formData) {
   console.log("Uploading image to:", API_URL);
-  const response = await axios.post(`${base}/api/outfit_searches/`, formData, {
+  const response = await axios.post(`${API_URL}/api/outfit_searches/`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
     withCredentials: false,
   });
